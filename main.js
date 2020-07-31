21cf5f29aaefb94443ed75c0852712e4203814a6
notforlife <69058071+notforlife@users.noreply.github.com>
2020-07-31 15:13:48 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-
 function clickOnBeeb() {
-  node = document.createElement("BUTTON")
-  node.outerHTML = "<button class="boring-button">Click me!</button>"
-  document.getElementById("fake-text").remove()
-  document.getElementById("column2").appendChild(node)
+  node = document.createElement("BUTTON");
+  node.outerHTML = "<button class="boring-button">Click me!</button>";
+  document.getElementById("column2").appendChild(node);
 
 }
