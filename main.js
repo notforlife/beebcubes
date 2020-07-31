f035926f4072209792bf64414410517af1ac46e1
notforlife <69058071+notforlife@users.noreply.github.com>
2020-07-31 15:23:57 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function clickOnBeeb() {
-  node = document.createElement("BUTTON");
+  
+  var node = document.createElement("BUTTON");
   node.outerHTML = "<button class="boring-button">Click me!</button>";
   document.getElementById("column2").appendChild(node);
 
