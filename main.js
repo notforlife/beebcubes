36d6cffb8e70c1eb3a10641b9c55a75305cf6dbb
notforlife <69058071+notforlife@users.noreply.github.com>
2020-07-31 19:46:43 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 var beebclicks = 0
 
 function clickOnBeeb() {
+  if (beebclicks < 100) {
   beebclicks ++
   var node = document.createElement("BUTTON");
   node.innerHTML = "Click Me";
   document.getElementById("column2").appendChild(node);
+  }
+  else {
+    var log = document.createElement("P");
+    log.innerHTML = "You have reached your maximum amount of displayable buttons. Research upgrades to increase this cap!"
+  }
 }
