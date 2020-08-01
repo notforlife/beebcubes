d4e2ef42b3bcad1f6879bb419516aac20d3dd9df
notforlife <69058071+notforlife@users.noreply.github.com>
2020-07-31 19:29:06 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+var beebclicks = 0
+
 function clickOnBeeb() {
+  beebclicks = beebclicks + 1
   var node = document.createElement("BUTTON");
   node.innerHTML = "Click Me";
   document.getElementById("column2").appendChild(node);
