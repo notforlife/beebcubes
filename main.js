a6dec5b246b167070be490c285360d27854eb584
notforlife <69058071+notforlife@users.noreply.github.com>
2020-08-01 14:33:25 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,22 +4,6 @@ var buttonCap = 30;
 var logMessages = [];
 var n;
 
-function clickOnBeeb() {
-  if (beebclicks < buttonCap) {
-  beebclicks ++
-  var node = document.createElement("BUTTON");
-  node.innerHTML = "Click Me";
-  document.getElementById("column2").appendChild(node);
-  }
-  else {
-    var log = document.createElement("P");
-    log.className = "message";
-    log.innerHTML = "You have reached your maximum amount of displayable buttons. Research upgrades to increase this cap!";
-    document.getElementById("log").appendChild(log);
-    logMessages.push(log.innerHTML);
-  }
-}
-
 function pushMessages(value) {
   
   var mlog = document.createElement("P");
@@ -43,4 +27,19 @@ function shiftMessages() {
   
 }
 
-shiftMessages()
+function clickOnBeeb() {
+  if (beebclicks < buttonCap) {
+  beebclicks ++
+  var node = document.createElement("BUTTON");
+  node.innerHTML = "Click Me";
+  document.getElementById("column2").appendChild(node);
+  }
+  else {
+    var log = document.createElement("P");
+    log.className = "message";
+    log.innerHTML = "You have reached your maximum amount of displayable buttons. Research upgrades to increase this cap!";
+    document.getElementById("log").appendChild(log);
+    logMessages.push(log.innerHTML);
+    shiftMessages()
+  }
+}
