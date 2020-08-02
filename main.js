cd4802fd070466cb14016c4b47c1b0da997a9702
notforlife <69058071+notforlife@users.noreply.github.com>
2020-08-02 08:45:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -9,14 +9,14 @@ var power = 0;
 var efficiency = 0.16
 
 function clickOnBeeb() {
+  beebclicks ++
   if (beebclicks < buttonCap) {
-  power ++
-  var node = document.createElement("BUTTON");
-  node.className = "button1"
-  node.innerHTML = "Click Me";
-  document.getElementById("column2").appendChild(node);
-  }
-  else {
+    power ++
+    var node = document.createElement("BUTTON");
+    node.className = "button1"
+    node.innerHTML = "Click Me";
+    document.getElementById("column2").appendChild(node);
+  } else {
     var log = document.createElement("P");
     log.className = "message";
     log.innerHTML = "You have reached your maximum amount of displayable buttons. Research upgrades to increase this cap!";
@@ -29,7 +29,7 @@ function runlock() {
 
   if (beebclicks >= 30) {
 
-    document.getElementsByClassName("column3").style.background-color = "rgb(202,202,202)";
+    document.getElementsByClassName("column3").style.color = "rgb(202,202,202)";
     document.getElementById("research").style.height = "30%"
     document.getElementById("upgrades").style.height = "65%"
     document.getElementById("research-header").innerHTML = "Research"
